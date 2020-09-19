7f9cfb982eb5f6b2fe1a590ac702291a909ad16b
Dominique Unruh <unruh@ut.ee>
2020-09-20 00:53:42 +0300

    Added an Isabelle thy for conveniently editing controlle_isabelle.ml

---
+++ Control_Isabelle.thy
@@ -0,0 +1,17 @@
+(*
+This theory is for opening \<open>control_isabelle.ml\<close> for editing. Load it in Isabelle and 
+control-click on "\<dots>/control_isabelle.ml" below
+*)
+
+theory Control_Isabelle
+  imports Main
+begin
+
+ML \<open>
+val inputPipeName = "/dev/null"
+val outputPipeName = "/dev/null"
+\<close>
+
+ML_file "src/main/resources/de/unruh/isabelle/control/control_isabelle.ml"
+
+end
