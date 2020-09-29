9b6830eb960ccf685fb13f7c1c1ed5f1e65f9d53
Dominique Unruh <unruh@ut.ee>
2020-09-29 18:29:26 +0300

    More patterns defined.

--- src/main/scala/de/unruh/isabelle/java/patterns/MatchManager.java
+++ src/main/scala/de/unruh/isabelle/java/patterns/MatchManager.java
@@ -4,6 +4,7 @@ import de.unruh.isabelle.java.patterns.Capture;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.function.Predicate;
 
 // DOCUMENT
 public final class MatchManager {
@@ -21,5 +22,33 @@ public final class MatchManager {
         for (Capture<?> capture : captured)
             capture.clear();
         captured.clear();
+    }
+
+    public boolean excursion(MatchExcursion excursion) {
+        return excursion(() -> { excursion.run(); return true; }, x -> true, false);
+    }
+
+
+    public <T> T excursion(MatchExcursionResult<T> excursion, Predicate<T> shouldReset, T failValue) {
+        int size = captured.size();
+        T result = failValue;
+        try {
+            result = excursion.run();
+            if (shouldReset.test(result)) throw new PatternMatchReject();
+        } catch (PatternMatchReject e) {
+            while (captured.size() > size) {
+                Capture<?> capture = captured.pop();
+                capture.clear();
+            }
+        }
+        return result;
+    }
+
+    public interface MatchExcursion {
+        void run() throws PatternMatchReject;
+    }
+
+    public interface MatchExcursionResult<T> {
+        T run() throws PatternMatchReject;
     }
 }
--- src/main/scala/de/unruh/isabelle/java/patterns/Patterns.java
+++ src/main/scala/de/unruh/isabelle/java/patterns/Patterns.java
@@ -7,25 +7,22 @@ import scala.Option;
 import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 // DOCUMENT, mention (somewhere): can access captures already in match, can fail match in action
-// TODO: Pattern for instance-match
-// TODO: Pattern for predicate-match
-// TODO: Pattern Or (with varargs)
-// TODO: NotMatch (negates the pattern, error if subpattern captures)
 // TODO: Make a separate library of all this
 // TODO: Can we handle exceptions better? (Avoid "throws Exception" clause)
 // TODO: Test cases
 public class Patterns {
-    public static <T,X> Case<T,X> withCase(Pattern<? super T> pattern, Callable<? extends X> action) {
+    public static <T, X> Case<T, X> withCase(Pattern<? super T> pattern, Callable<? extends X> action) {
         return new Case<>(pattern, action);
     }
 
     @SafeVarargs
-    public static <T,X> X match(T value, Case<T,X> ... cases) throws Exception {
+    public static <T, X> X match(T value, Case<T, X>... cases) throws Exception {
         MatchManager mgr = new MatchManager();
-        for (Case<T,X> cas : cases) {
+        for (Case<T, X> cas : cases) {
             Option<X> result = cas.apply(mgr, value);
             if (result.nonEmpty())
                 return result.get();
@@ -33,22 +30,25 @@ public class Patterns {
         throw new MatchError(value);
     }
 
-    public static <T,X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1) throws Exception {
+    public static <T, X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1) throws Exception {
         return match(value, withCase(pattern1, action1));
     }
-    public static <T,X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
-                                Pattern<? super T> pattern2, Callable<? extends X> action2) throws Exception {
+
+    public static <T, X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
+                                 Pattern<? super T> pattern2, Callable<? extends X> action2) throws Exception {
         return match(value, withCase(pattern1, action1), withCase(pattern2, action2));
     }
-    public static <T,X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
-                                Pattern<? super T> pattern2, Callable<? extends X> action2,
-                                Pattern<? super T> pattern3, Callable<? extends X> action3) throws Exception {
+
+    public static <T, X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
+                                 Pattern<? super T> pattern2, Callable<? extends X> action2,
+                                 Pattern<? super T> pattern3, Callable<? extends X> action3) throws Exception {
         return match(value, withCase(pattern1, action1), withCase(pattern2, action2), withCase(pattern3, action3));
     }
-    public static <T,X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
-                                Pattern<? super T> pattern2, Callable<? extends X> action2,
-                                Pattern<? super T> pattern3, Callable<? extends X> action3,
-                                Pattern<? super T> pattern4, Callable<? extends X> action4) throws Exception {
+
+    public static <T, X> X match(T value, Pattern<? super T> pattern1, Callable<? extends X> action1,
+                                 Pattern<? super T> pattern2, Callable<? extends X> action2,
+                                 Pattern<? super T> pattern3, Callable<? extends X> action3,
+                                 Pattern<? super T> pattern4, Callable<? extends X> action4) throws Exception {
         return match(value, withCase(pattern1, action1), withCase(pattern2, action2), withCase(pattern3, action3),
                 withCase(pattern4, action4));
     }
@@ -63,14 +63,15 @@ public class Patterns {
 
             @Override
             public String toString() {
-                return "="+expected;
+                return "=" + expected;
             }
         };
     }
 
     public static final Pattern<Object> Any = new Pattern<>() {
         @Override
-        public void apply(MatchManager mgr, Object value) {}
+        public void apply(MatchManager mgr, Object value) {
+        }
 
         @Override
         public String toString() {
@@ -81,7 +82,7 @@ public class Patterns {
     public static final Pattern<Object> Null = new Pattern<>() {
         @Override
         public void apply(MatchManager mgr, Object value) throws PatternMatchReject {
-            if (value != null) Pattern.reject();
+            if (value != null) reject();
         }
 
         @Override
@@ -90,11 +91,11 @@ public class Patterns {
         }
     };
 
-    public static <T> Pattern<T> NotNull(Pattern<T> pattern) {
+    public static <T> Pattern<T> NotNull(Pattern<? super T> pattern) {
         return new Pattern<>() {
             @Override
             public void apply(MatchManager mgr, T value) throws PatternMatchReject {
-                if (value == null) Pattern.reject();
+                if (value == null) reject();
                 pattern.apply(mgr, value);
             }
 
@@ -106,20 +107,92 @@ public class Patterns {
     }
 
     @SafeVarargs
-    public static <T> Pattern<T> And(Pattern<T> ... patterns) {
-        return new Pattern<T>() {
+    public static <T> Pattern<T> And(Pattern<? super T>... patterns) {
+        return new Pattern<>() {
             @Override
             public void apply(MatchManager mgr, T value) throws PatternMatchReject {
-                for (Pattern<T> pattern : patterns)
+                for (Pattern<? super T> pattern : patterns)
                     pattern.apply(mgr, value);
             }
 
             @Override
             public String toString() {
                 StringJoiner joiner = new StringJoiner(", ");
-                for (Pattern<T> pattern : patterns)
+                for (Pattern<?> pattern : patterns)
                     joiner.add(pattern.toString());
-                return "And("+joiner+")";
+                return "And(" + joiner + ")";
+            }
+        };
+    }
+
+    @SafeVarargs
+    public static <T> Pattern<T> Or(Pattern<? super T>... patterns) {
+        return new Pattern<T>() {
+            @Override
+            public void apply(MatchManager mgr, T value) throws PatternMatchReject {
+                if (patterns.length == 0) reject();
+                for (int i=0; i<patterns.length-1; i++) {
+                    Pattern<? super T> pattern = patterns[i];
+                    if (mgr.excursion(() -> pattern.apply(mgr, value))) return;
+                }
+                patterns[patterns.length-1].apply(mgr, value);
+            }
+
+            @Override
+            public String toString() {
+                StringJoiner joiner = new StringJoiner(", ");
+                for (Pattern<?> pattern : patterns)
+                    joiner.add(pattern.toString());
+                return "Or(" + joiner + ")";
+            }
+        };
+    }
+
+    public static <T,U> Pattern<T> Instance(Class<U> clazz, Pattern<? super U> pattern) {
+        return new Pattern<T>() {
+            @Override
+            public void apply(MatchManager mgr, T value) throws PatternMatchReject {
+                U castValue = null;
+                try {
+                    castValue = clazz.cast(value);
+                } catch (ClassCastException e) {
+                    reject();
+                }
+                pattern.apply(mgr,castValue);
+            }
+
+            @Override
+            public String toString() {
+                return "Instance("+clazz.getSimpleName()+","+pattern+")";
+            }
+        };
+    }
+
+    public static <T> Pattern<T> Pred(Predicate<? super T> predicate) {
+        return new Pattern<T>() {
+            @Override
+            public void apply(MatchManager mgr, T value) throws PatternMatchReject {
+                if (!predicate.test(value)) reject();
+            }
+
+            @Override
+            public String toString() {
+                return "Pred(...)";
+            }
+        };
+    }
+
+    public static <T> Pattern<T> NoMatch(Pattern<? super T> pattern) {
+        return new Pattern<T>() {
+            @Override
+            public void apply(MatchManager mgr, T value) throws PatternMatchReject {
+                boolean matched = mgr.excursion(() -> pattern.apply(mgr, value));
+                if (matched) reject();
+            }
+
+            @Override
+            public String toString() {
+                return "NoMatch("+pattern+")";
             }
         };
     }
