73bd385723c139fcedfb5b70e9db00767190a0f7
Dominique Unruh <unruh@ut.ee>
2020-10-10 21:53:02 +0300

    Fixed travis-setup.sh (did not detect already downloaded Isabelle)

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -10,19 +10,19 @@ if ! [ -e ~/install/sbt ]; then
 fi
 chmod +x ~/install/sbt
 
-if ! [ -e /opt/Isabelle$ISA ]; then
+case "$TRAVIS_OS_NAME" in
+  linux) ISABELLE_HOME=~/install/Isabelle$ISA;;
+  osx) ISABELLE_HOME=~/install/Isabelle$ISA.app/Isabelle;;
+  *) echo "Unsupported OS: $TRAVIS_OS_NAME"; exit 1;;
+esac
+
+if ! [ -e "$ISABELLE_HOME" ]; then
   case "$TRAVIS_OS_NAME" in
     linux) curl https://isabelle.in.tum.de/website-Isabelle$ISA/dist/Isabelle${ISA}_linux.tar.gz | tar -x -z -C ~/install;;
     osx) curl https://isabelle.in.tum.de/website-Isabelle$ISA/dist/Isabelle${ISA}_macos.tar.gz | tar -x -z -C ~/install;;
     *) echo "Unsupported OS: $TRAVIS_OS_NAME"; exit 1;;
   esac
 fi
-
-case "$TRAVIS_OS_NAME" in
-  linux) ISABELLE_HOME=~/install/Isabelle$ISA;;
-  osx) ISABELLE_HOME=~/install/Isabelle$ISA.app/Isabelle;;
-  *) echo "Unsupported OS: $TRAVIS_OS_NAME"; exit 1;;
-esac
 
 echo "$ISABELLE_HOME" > .isabelle-home
 
