5bce746f2bcec70f992df8b17dd04cc6bd16e7ba
Dominique Unruh <unruh@ut.ee>
2020-10-18 19:35:35 +0300

    Removed abbreviation JPatterns.P (makes Scaladoc confusing). Removed pattern Abs(name:String, ...). Added pattern Var(name:String, ...). Added patterns TFree, TVar.

--- src/main/scala/de/unruh/isabelle/java/JPatterns.scala
+++ src/main/scala/de/unruh/isabelle/java/JPatterns.scala
@@ -1,6 +1,6 @@
 package de.unruh.isabelle.java
 
-import de.unruh.isabelle.pure.{Abs, App, Bound, Const, Free, Term, Typ, Type, Var}
+import de.unruh.isabelle.pure.{Abs, App, Bound, Const, Free, TFree, TVar, Term, Typ, Type, Var}
 import de.unruh.javapatterns.Patterns.Is
 import de.unruh.javapatterns.{MatchManager, Pattern, PatternMatchReject}
 import org.jetbrains.annotations.{NotNull, Nullable}
@@ -34,13 +34,10 @@ import scala.annotation.varargs
  * are [[JPatterns.Type(name:d* Type]], [[JPatterns.TFree(name:d* TFree]], [[JPatterns.TVar(name:d* TVar]].
  * */
 object JPatterns {
-  // TODO inline
-  type P[A] = Pattern[_ >: A]
-
   /** Pattern matching a constant ([[pure.Const Const]]) `c`. Subpattern `name` will be applied to `c.`[[pure.Const.name name]],
    * and `typ` will be applied to `c.`[[pure.Const.typ typ]].
    **/
-  @NotNull def Const(@NotNull name: P[String], @NotNull typ: P[Typ]): Pattern[Term] = new Pattern[Term]() {
+  @NotNull def Const(@NotNull name: Pattern[_ >: String], @NotNull typ: Pattern[_ >: Typ]): Pattern[Term] = new Pattern[Term]() {
     @throws[PatternMatchReject]
     override def apply(@NotNull mgr: MatchManager, @Nullable term: Term): Unit = term match {
       case Const(n, t) =>
@@ -90,14 +87,10 @@ object JPatterns {
     override def toString: String = s"Abs($name,$typ,$body)"
   }
 
-  // DOCUMENT
-  @NotNull def Abs(@NotNull name: String, @NotNull typ: Pattern[_ >: Typ], @NotNull body: Pattern[_ >: Term]): Pattern[Term] =
-    Abs(Is(name), typ, body)
-
   /** Pattern matching a free variable ([[pure.Free Free]]) `v`. Subpattern `name` will be applied to `v.`[[pure.Free.name name]],
    * and `typ` will be applied to `v.`[[pure.Free.typ typ]].
    **/
-  @NotNull def Free(@NotNull name: P[String], @NotNull typ: P[Typ]): Pattern[Term] = new Pattern[Term] {
+  @NotNull def Free(@NotNull name: Pattern[_ >: String], @NotNull typ: Pattern[_ >: Typ]): Pattern[Term] = new Pattern[Term] {
     override def apply(@NotNull mgr: MatchManager, @Nullable term: Term): Unit = term match {
       case Free(n, t) =>
         name(mgr, n)
@@ -109,14 +102,14 @@ object JPatterns {
     override def toString: String = s"Free($name,$typ)"
   }
 
-  // DOCUMENT
-  @NotNull def Free(@NotNull name: String, @NotNull typ: P[Typ]): Pattern[Term] = Free(Is(name), typ)
+  /** Short for `Free(Is(name),typ)`. That is, matches a free variable with name `name`. */
+  @NotNull def Free(@NotNull name: String, @NotNull typ: Pattern[_ >: Typ]): Pattern[Term] = Free(Is(name), typ)
 
   /** Pattern matching a schematic variable ([[pure.Var Var]]) `v`. Subpattern `name` will be applied to `v.`[[pure.Var.name name]],
    * `index` will be applied to `v.`[[pure.Var.index index]],
    * and `typ` will be applied to `v.`[[pure.Free.typ typ]].
    **/
-  @NotNull def Var(@NotNull name: P[String], @NotNull index: P[Int], @NotNull typ: P[Typ]): Pattern[Term] = new Pattern[Term] {
+  @NotNull def Var(@NotNull name: Pattern[_ >: String], @NotNull index: Pattern[_ >: Int], @NotNull typ: Pattern[_ >: Typ]): Pattern[Term] = new Pattern[Term] {
     override def apply(@NotNull mgr: MatchManager, @Nullable term: Term): Unit = term match {
       case Var(n, i, t) =>
         name(mgr, n)
@@ -129,10 +122,15 @@ object JPatterns {
     override def toString: String = s"Var($name,$index,$typ)"
   }
 
+  /** Short for `Var(Is(name),Is(index),typ)`. That is, matches a schematic variable with name `name` and index `index`. */
+  @NotNull def Var(@NotNull name: String, index: Int, @NotNull typ: Pattern[_ >: Typ]): Pattern[Term] =
+    Var(Is(name), Is(index), typ)
+
+
   /** Pattern matching a bound (de Bruijn) variable ([[pure.Bound Bound]]) `v`. Subpattern `index` will be applied to
    * `v.`[[pure.Bound.index index]].
    **/
-  @NotNull def Bound(@NotNull index: P[Int]): Pattern[Term] = new Pattern[Term] {
+  @NotNull def Bound(@NotNull index: Pattern[_ >: Int]): Pattern[Term] = new Pattern[Term] {
     override def apply(@NotNull mgr: MatchManager, @Nullable term: Term): Unit = term match {
       case Bound(i) =>
         index(mgr, i)
@@ -143,14 +141,14 @@ object JPatterns {
     override def toString: String = s"Bound$index"
   }
 
-  // DOCUMENT
+  /** Short for `Bound(Is(index))`. That is, matches a constant with index `index`. */
   @NotNull def Bound(index: Int): Pattern[Term] = Bound(Is(index))
 
   /** Pattern matching a type constructor ([[pure.Type Type]]) `t`. Subpattern `name` will be applied to
    * `t.`[[pure.Type.name name]], and the ''n'' subpatterns `args` will be applied to the ''n'' type
    * arguments `t.`[[pure.Type.args]]. (If `t` does not have ''n'' type arguments, the pattern does not match.)
    **/
-  @NotNull @varargs def Type(@NotNull name: P[String], @NotNull args: P[Typ]*): Pattern[Typ] = new Pattern[Typ] {
+  @NotNull @varargs def Type(@NotNull name: Pattern[_ >: String], @NotNull args: Pattern[_ >: Typ]*): Pattern[Typ] = new Pattern[Typ] {
     override def apply(@NotNull mgr: MatchManager, @Nullable typ: Typ): Unit = typ match {
       case Type(n, a@_*) =>
         if (a.length != args.length) Pattern.reject()
@@ -162,12 +160,12 @@ object JPatterns {
     override def toString: String = s"Type($name,${args.mkString(",")})"
   }
 
-  // DOCUMENT
-  @NotNull @varargs def Type(@NotNull name: String, @NotNull args: P[Typ]*): Pattern[Typ] =
+  /** Short for `Type(Is(name),args)`. That is, matches a type with type constructor `name`. */
+  @NotNull @varargs def Type(@NotNull name: String, @NotNull args: Pattern[_ >: Typ]*): Pattern[Typ] =
     Type(Is(name), args: _*)
 
   // DOCUMENT
-  @NotNull def Type(@NotNull name: P[String], @NotNull args: P[Array[Typ]]): Pattern[Typ] = new Pattern[Typ] {
+  @NotNull def Type(@NotNull name: Pattern[_ >: String], @NotNull args: Pattern[_ >: Array[Typ]]): Pattern[Typ] = new Pattern[Typ] {
     override def apply(@NotNull mgr: MatchManager, @Nullable typ: Typ): Unit = typ match {
       case Type(n, a@_*) =>
         name(mgr, n)
@@ -177,5 +175,43 @@ object JPatterns {
     override def toString: String = s"Type($name,$args*)"
   }
 
-  // TODO: add TFree, TVar
+
+  /** Pattern matching a free type variable ([[pure.TFree TFree]]) `v`. Subpattern `name` will be applied to `v.`[[pure.TFree.name name]],
+   * and `sort` will be applied to `v.`[[pure.TFree.sort sort]]
+   **/
+  @NotNull def TFree(@NotNull name: Pattern[_ >: String], @NotNull sort: Pattern[_ >: Array[String]]): Pattern[Typ] = new Pattern[Typ] {
+    override def apply(@NotNull mgr: MatchManager, @Nullable typ: Typ): Unit = typ match {
+      case TFree(n, s) =>
+        name(mgr, n)
+        sort(mgr, s.toArray)
+      case _ =>
+        Pattern.reject()
+    }
+
+    override def toString: String = s"TFree($name,$sort)"
+  }
+
+  /** Short for `TFree(Is(name),sort)`. That is, matches a free type variable with name `name`. */
+  @NotNull def TFree(@NotNull name: String, @NotNull sort: Pattern[_ >: Array[String]]): Pattern[Typ] = TFree(Is(name), sort)
+
+  /** Pattern matching a schematic type variable ([[pure.TVar TVar]]) `v`. Subpattern `name` will be applied to `v.`[[pure.TVar.name name]],
+   * `index` will be applied to `v.`[[pure.TVar.index index]],
+   * and `sort` will be applied to `v.`[[pure.TVar.sort sort]]
+   **/
+  @NotNull def TVar(@NotNull name: Pattern[_ >: String], @NotNull index: Pattern[_ >: Int], @NotNull sort: Pattern[_ >: Array[String]]): Pattern[Typ] = new Pattern[Typ] {
+    override def apply(@NotNull mgr: MatchManager, @Nullable typ: Typ): Unit = typ match {
+      case TVar(n, i, s) =>
+        name(mgr, n)
+        index(mgr, i)
+        sort(mgr, s.toArray)
+      case _ =>
+        Pattern.reject()
+    }
+
+    override def toString: String = s"TVar($name,$index,$sort)"
+  }
+
+  /** Short for `Var(Is(name),Is(index),typ)`. That is, matches a schematic variable with name `name` and index `index`. */
+  @NotNull def TVar(@NotNull name: String, index: Int, @NotNull sort: Pattern[_ >: Array[String]]): Pattern[Typ] =
+    TVar(Is(name), Is(index), sort)
 }
