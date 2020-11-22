a324f8006db05e53746d04ea56c145b450134097
Dominique Unruh <unruh@ut.ee>
2020-11-22 10:31:30 +0200

    Added: Context.setMode.

--- src/main/scala/de/unruh/isabelle/pure/Context.scala
+++ src/main/scala/de/unruh/isabelle/pure/Context.scala
@@ -10,6 +10,7 @@ import Context.Ops
 
 // Implicits
 import de.unruh.isabelle.pure.Implicits.theoryConverter
+import de.unruh.isabelle.mlvalue.Implicits._
 
 /** Represents a proof context (ML type `Proof.context`) in the Isabelle process.
  *
@@ -50,16 +51,34 @@ final class Context private [Context](val mlValue : MLValue[Context]) extends ML
 
   /** Returns the theory underlying this context. */
   def theoryOf(implicit isabelle: Isabelle, executionContext: ExecutionContext) : Theory = Ops.theoryOf(this).retrieveNow
+
+  /** Sets the "inner syntax mode" of the context.
+   * This affects, e.g., parsing of terms/types.
+   * (Corresponds loosely to Proof_Context.set_mode in Isabelle/ML.)
+   *
+   * @param mode the mode to enable
+   * @return a new context with the corresponding mode
+   */
+  def setMode(mode: Context.Mode)
+             (implicit isabelle: Isabelle, executionContext: ExecutionContext) : Context =
+    Ops.setMode(this, mode.id).retrieveNow
 }
 
 object Context extends MLValueWrapper.Companion[Context] {
   override protected def newOps(implicit isabelle: Isabelle, ec: ExecutionContext): Ops = new Ops()
   //noinspection TypeAnnotation
-  protected[isabelle] class Ops(implicit val isabelle: Isabelle, ec: ExecutionContext) extends super.Ops {
+  protected class Ops(implicit val isabelle: Isabelle, ec: ExecutionContext) extends super.Ops {
     import MLValue.compileFunction
+
     lazy val contextFromTheory : MLFunction[Theory, Context] =
       compileFunction[Theory, Context]("Proof_Context.init_global")
+
     lazy val theoryOf = compileFunction[Context, Theory]("Proof_Context.theory_of")
+
+    lazy val setMode = compileFunction[Context, Int, Context](s"""fn (ctxt, id) => Proof_Context.set_mode
+       (case id of ${Mode.schematic.id} => Proof_Context.mode_schematic
+                 | ${Mode.pattern.id} => Proof_Context.mode_pattern
+                 | ${Mode.abbrev.id} => Proof_Context.mode_abbrev) ctxt""")
   }
 
   /** Initializes a new context from the Isabelle theory `theory` */
@@ -78,4 +97,16 @@ object Context extends MLValueWrapper.Companion[Context] {
   override protected val predefinedException: String = "E_Context"
 
   override protected def instantiate(mlValue: MLValue[Context]): Context = new Context(mlValue)
+
+  /** Inner syntax mode of a context. (Corresponds to Proof_Context.mode in Isabelle/ML.)
+   * Possible values are [[Mode.pattern]], [[Mode.schematic]], [[Mode.abbrev]]. */
+  sealed abstract class Mode(private[Context] val id: Int)
+  object Mode {
+    /** "pattern binding schematic variables" according to Isabelle source comments */
+    case object pattern extends Mode(1)
+    /** "term referencing loose schematic variables" according to Isabelle source comments */
+    case object schematic extends Mode(2)
+    /** "abbrev mode – no normalization" according to Isabelle source comments */
+    case object abbrev extends Mode(3)
+  }
 }
