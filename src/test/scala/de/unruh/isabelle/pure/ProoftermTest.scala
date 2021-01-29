15749b7369fc26e6633c08f836fa89e5ac6a2ddb
Dominique Unruh <unruh@ut.ee>
2021-01-29 23:09:55 +0200

    ProofTermTest: Not using "assume" for ignored tests.

--- src/test/scala/de/unruh/isabelle/pure/ProoftermTest.scala
+++ src/test/scala/de/unruh/isabelle/pure/ProoftermTest.scala
@@ -11,17 +11,17 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class ProoftermTest extends AnyFunSuite {
 
   test("old version") {
-    assume(!Version.from2020)
-
-    val exn = intercept[IsabelleException] { PThm(Thm(Context("Main"), "HOL.refl")).proof }
-    assert(exn.message.startsWith("Proofterms are supported only for Isabelle >=2020, not "))
+    if (!Version.from2020) {
+      val exn = intercept[IsabelleException] { PThm(Thm(Context("Main"), "HOL.refl")).proof }
+      assert(exn.message.startsWith("Proofterms are supported only for Isabelle >=2020, not "))
+    }
   }
 
   test("get refl") {
-    assume(Version.from2020)
-
-    val ctxt = Context("Main")
-    val prf = PThm(Thm(ctxt, "HOL.refl")).proof
-    print(prf)
+    if (Version.from2020) {
+      val ctxt = Context("Main")
+      val prf = PThm(Thm(ctxt, "HOL.refl")).proof
+      print(prf)
+    }
   }
 }
