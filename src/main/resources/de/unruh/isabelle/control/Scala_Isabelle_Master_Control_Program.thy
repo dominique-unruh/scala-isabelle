56479846b8beb431682fe9859a00c9740ab3f2fe
Dominique Unruh <unruh@ut.ee>
2021-11-11 16:40:08 +0200

    Fixed code for setting working directory: Converting to platform path before calling chDir. (Otherwise it fails on Windows)

--- src/main/resources/de/unruh/isabelle/control/Scala_Isabelle_Master_Control_Program.thy
+++ src/main/resources/de/unruh/isabelle/control/Scala_Isabelle_Master_Control_Program.thy
@@ -4,7 +4,14 @@ begin
 
 ML_file "control_isabelle.ml"
 
-ML \<open>OS.FileSys.chDir "WORKING_DIRECTORY"\<close>
+ML \<open>
+let
+val path = File.platform_path (Path.explode "WORKING_DIRECTORY")
+in
+  OS.FileSys.chDir path
+  handle OS.SysErr (msg,_) => error ("Could set Isabelle working directory as " ^ path ^ ": " ^ msg)
+end
+\<close>
 
 ML "Control_Isabelle.update_ml_compilation_context (fn _ => Context.Theory \<^theory>)"
 
