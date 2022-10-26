315cb1061a00abc2739307e72704a4de08999567
Dominique Unruh <unruh@ut.ee>
2022-10-26 09:28:33 +0300

    Enabled Isabelle2022.

--- src/test/scala/de/unruh/isabelle/mlvalue/VersionTest.scala
+++ src/test/scala/de/unruh/isabelle/mlvalue/VersionTest.scala
@@ -15,35 +15,35 @@ class VersionTest extends org.scalatest.funsuite.AnyFunSuite {
     println(triple)
     string match {
       case "Isabelle2020: April 2020" =>
-        assert(triple == (2020,0,NOT_RC))
+        assert(triple == (2020, 0, NOT_RC))
       case "Isabelle2019: June 2019" =>
-        assert(triple == (2019,0,NOT_RC))
+        assert(triple == (2019, 0, NOT_RC))
       case "Isabelle2021-RC0: November 2020" =>
-        assert(triple == (2021,0,0))
+        assert(triple == (2021, 0, 0))
       case "Isabelle2021-RC2: January 2021" =>
-        assert(triple == (2021,0,2))
+        assert(triple == (2021, 0, 2))
       case "Isabelle2021-RC3: January 2021" =>
-        assert(triple == (2021,0,3))
+        assert(triple == (2021, 0, 3))
       case "Isabelle2021-RC4: February 2021" =>
-        assert(triple == (2021,0,4))
+        assert(triple == (2021, 0, 4))
       case "Isabelle2021-RC5: February 2021" =>
-        assert(triple == (2021,0,5))
+        assert(triple == (2021, 0, 5))
       case "Isabelle2021-RC6: February 2021" =>
-        assert(triple == (2021,0,6))
+        assert(triple == (2021, 0, 6))
       case "Isabelle2021: February 2021" =>
-        assert(triple == (2021,0,NOT_RC))
+        assert(triple == (2021, 0, NOT_RC))
       case "Isabelle2021-1-RC1" =>
-        assert(triple == (2021,1,1))
+        assert(triple == (2021, 1, 1))
       case "Isabelle2021-1-RC2" =>
-        assert(triple == (2021,1,2))
+        assert(triple == (2021, 1, 2))
       case "Isabelle2021-1-RC3" =>
-        assert(triple == (2021,1,3))
+        assert(triple == (2021, 1, 3))
       case "Isabelle2021-1-RC4" =>
-        assert(triple == (2021,1,4))
+        assert(triple == (2021, 1, 4))
       case "Isabelle2021-1-RC5" =>
-        assert(triple == (2021,1,5))
+        assert(triple == (2021, 1, 5))
       case "Isabelle2021-1" =>
-        assert(triple == (2021,1,NOT_RC))
+        assert(triple == (2021, 1, NOT_RC))
       case "Isabelle2022-RC0" =>
         assert(triple == (2022, 0, 0))
       case "Isabelle2022-RC1" =>
@@ -54,6 +54,8 @@ class VersionTest extends org.scalatest.funsuite.AnyFunSuite {
         assert(triple == (2022, 0, 3))
       case "Isabelle2022-RC4" =>
         assert(triple == (2022, 0, 4))
+      case "Isabelle2022" =>
+        assert(triple == (2022, 0, NOT_RC))
       case _ =>
         fail(s"Unknown version string $string. Please extend test case")
     }
