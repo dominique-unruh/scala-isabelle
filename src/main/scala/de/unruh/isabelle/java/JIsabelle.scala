408e6a6f3ca30a1c2e42fe671eaab26191d76a6d
Dominique Unruh <unruh@ut.ee>
2023-09-11 20:31:48 +0300

    Added more `JIsabelle.setupSetXXX` methods.
    Resolves #10.

--- src/main/scala/de/unruh/isabelle/java/JIsabelle.scala
+++ src/main/scala/de/unruh/isabelle/java/JIsabelle.scala
@@ -4,6 +4,7 @@ import java.nio.file.Path
 
 import de.unruh.isabelle.control
 import de.unruh.isabelle.control.Isabelle
+import scala.jdk.CollectionConverters._
 
 /**
  * This object contains utility methods for invoking scala-isabelle methods from Java
@@ -24,4 +25,36 @@ object JIsabelle {
    * @param build the new value for `setup.`[[de.unruh.isabelle.control.Isabelle.Setup.build build]]
    **/
   def setupSetBuild(build : Boolean, setup : Isabelle.Setup): Isabelle.Setup = setup.copy(build = build)
+
+  /** Sets the [[de.unruh.isabelle.control.Isabelle.Setup.verbose verbose]] flag in the setup `setup`.
+   *
+   * @return `setup` with [[de.unruh.isabelle.control.Isabelle.Setup.verbose verbose]] set to `verbose`
+   * @param verbose the new value for `setup.`[[de.unruh.isabelle.control.Isabelle.Setup.verbose verbose]]
+   * */
+  def setupSetVerbose(verbose : Boolean, setup : Isabelle.Setup): Isabelle.Setup = setup.copy(verbose = verbose)
+
+  /** Sets the [[de.unruh.isabelle.control.Isabelle.Setup.userDir userDir]] directory in the setup `setup`.
+   * Note: There is no way to change the `userDir` to `None`. However, the default value for `userDir` is `None`.
+   *
+   * @return `setup` with [[de.unruh.isabelle.control.Isabelle.Setup.userDir userDir]] set to `Some(userDir)`
+   * @param verbose the new value for `setup.`[[de.unruh.isabelle.control.Isabelle.Setup.verbose verbose]]
+   * */
+  def setupSetUserDir(userDir: Path, setup: Isabelle.Setup): Isabelle.Setup = setup.copy(userDir = Some(userDir))
+
+  /** Sets the [[de.unruh.isabelle.control.Isabelle.Setup.workingDirectory workingDirectory]] directory in the setup `setup`.
+   *
+   * @return `setup` with [[de.unruh.isabelle.control.Isabelle.Setup.workingDirectory workingDirectory]] set to `workingDirectory`
+   * @param workingDirectory the new value for `setup.`[[de.unruh.isabelle.control.Isabelle.Setup.workingDirectory workingDirectory]]
+   * */
+  def setupSetWorkingDirectory(workingDirectory: Path, setup: Isabelle.Setup): Isabelle.Setup =
+    setup.copy(workingDirectory = workingDirectory)
+
+  /** Sets the [[de.unruh.isabelle.control.Isabelle.Setup.sessionRoots sessionRoots]] directories in the setup `setup`.
+   *
+   * @return `setup` with [[de.unruh.isabelle.control.Isabelle.Setup.sessionRoots sessionRoots]] set to `sessionRoots`
+   * @param sessionRoots the new value for `setup.`[[de.unruh.isabelle.control.Isabelle.Setup.sessionRoots sessionRoots]].
+   *                     It will automatically be converted to a Scala [[Seq]].
+   * */
+  def setupSetSessionRoots(sessionRoots: java.lang.Iterable[Path], setup: Isabelle.Setup): Isabelle.Setup =
+    setup.copy(sessionRoots = sessionRoots.asScala.toSeq)
 }
