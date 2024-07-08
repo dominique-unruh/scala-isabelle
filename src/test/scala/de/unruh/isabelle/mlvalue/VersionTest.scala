799d799fab12b3e98d0700251ae40425b148a798
Dominique Unruh <qis.server.config.ian6jk@rwth.unruh.de>
2024-07-08 09:53:49 +0200

    Added version 2024 to the library. (Nothing fixed yet. Doesn't work.)

--- src/test/scala/de/unruh/isabelle/mlvalue/VersionTest.scala
+++ src/test/scala/de/unruh/isabelle/mlvalue/VersionTest.scala
@@ -68,6 +68,8 @@ class VersionTest extends org.scalatest.funsuite.AnyFunSuite {
         assert(triple == (2023, 0, 5))
       case "Isabelle2023" =>
         assert(triple == (2023, 0, NOT_RC))
+      case "Isabelle2024" =>
+        assert(triple == (2024, 0, NOT_RC))
       case _ =>
         fail(s"Unknown version string $string. Please extend test case")
     }
